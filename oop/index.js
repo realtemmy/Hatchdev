a5ea5b931443f9a90c161afbb2c4f8c0185ca177
Temiloluwa <temiloluwaOgunti8@gmail.com>
2025-05-31 11:43:45 +0100

    Array functions

--- oop/index.js
+++ oop/index.js
@@ -1,5 +1,5 @@
 // Write 10 array functions in javascript
-// length, push, pop, shift, unshift,indexOf, join, includes, reverse, splice
+// length, push, pop, shift, unshift,indexOf, join, includes, reverse, splice, filter, foreach, map, slice
 
 class arrayFunctions {
   constructor(value) {
@@ -39,11 +39,91 @@ class arrayFunctions {
     this.array = newArr;
     return first;
   }
+  indexOf(value) {
+    for (let i = 0; i < this.array.length; i++) {
+      if (this.array[i] === value) {
+        return i;
+      }
+    }
+    return -1;
+  }
+  includes(value) {
+    for (let i = 0; i < this.array.length; i++) {
+      if (this.array[i] === value) {
+        return true;
+      }
+    }
+    return false;
+  }
+  reverse() {
+    if (this.array.length === 0) return this.array;
+    
+    let left = 0,
+      right = this.array.length - 1;
+    while (left < right) {
+      // Swap
+      let temp = this.array[left];
+      this.array[left] = this.array[right];
+      this.array[right] = temp;
+
+      left++;
+      right--;
+    }
+
+    return this.array;
+  }
+
+  forEach(cb) {
+    for (let i = 0; i < this.array.length; i++) {
+      cb(this.array[i]);
+    }
+
+    function cb(element) {
+      console.log(element);
+    }
+  }
+
+  map(cb) {
+    const newArr = [];
+    for (let i = 0; i < this.array.length; i++) {
+      const newEL = cb(this.array[i]);
+      newArr.push(newEL);
+    }
+
+    return newArr;
+  }
+  filter(cb) {
+    const newArr = [];
+    for (let i = 0; i < this.array.length; i++) {
+      if (cb(this.array[i])) {
+        newArr.push(this.array[i]);
+      }
+    }
+    return newArr;
+  }
+  find(cb) {
+    for (let i = 0; i < this.array.length; i++) {
+      if (cb(this.array[i])) {
+        return this.array[i];
+      }
+    }
+
+    return [];
+  }
+  slice(start, end = this.array.length) {
+    if (start < 0) start += this.array.length;
+  }
 }
 
 const myFunc = new arrayFunctions();
-// console.log(myFunc.length("name"));
-console.log(myFunc.push("tobi"));
-console.log(myFunc.push("temi"));
-// console.log(myFunc.pop());
-console.log(myFunc.shift());
+myFunc.push("Temi");
+myFunc.push("Tobi");
+myFunc.push("Adebisi");
+myFunc.push("Oreoluwa");
+// console.log(myFunc.indexOf("Temi"))
+// console.log(myFunc.includes("Temi"));
+console.log(myFunc.reverse())
+// console.log(myFunc.forEach());
+// console.log(myFunc.map((element) => element + "s"));
+// console.log(myFunc.filter((word) => word.length > 3));
+// console.log(myFunc.find((word) => word.length >= 7));
--- oop/index.ts
+++ oop/index.ts
@@ -2,14 +2,111 @@
 
 class arrFunc {
   value: string;
-    array: string[];
+  array: string[];
 
   constructor(value: string) {
     this.value = value;
+    this.array = [];
   }
 
   // Methods
-  push(value: string){
+  push(value: string): string[] {
+    this.array[this.array.length] = value;
+    return this.array;
+  }
+  pop(): string | undefined {
+    const newArr = [];
+    let last;
+    for (let i = 0; i < this.array.length; i++) {
+      if (i === this.array.length - 1) {
+        last = this.array[i];
+        break;
+      }
+      newArr[i] = this.array[i];
+    }
 
+    this.array = newArr;
+
+    return last;
+  }
+  shift(): string  {
+    const first = this.array[0];
+    const newArr = [];
+    for (let i = 1; i < this.array.length; i++) {
+      newArr[i - 1] = this.array[i];
+    }
+    this.array = newArr;
+    return first;
+  }
+  indexOf(value: string): number {
+    for (let i = 0; i < this.array.length; i++) {
+      if (this.array[i] === value) {
+        return i;
+      }
+    }
+    return -1;
+  }
+  includes(value: string): boolean {
+    for (let i = 0; i < this.array.length; i++) {
+      if (this.array[i] === value) {
+        return true;
+      }
+    }
+    return false;
+  }
+  reverse() {
+    let left = 0,
+      right = this.array.length - 1;
+    while (left < right) {
+      // Swap
+      let temp = this.value[left];
+      this.value[left] = this.array[right];
+      this.value[right] = temp;
+
+      left++;
+      right--;
+    }
+
+    return this.value;
+  }
+  forEach(cb) {
+    for (let i = 0; i < this.array.length; i++) {
+      cb(this.array[i]);
+    }
+
+    function cb(element) {
+      console.log(element);
+    }
+  }
+
+  map(cb) {
+    const newArr = [];
+    for (let i = 0; i < this.array.length; i++) {
+      const newEL = cb(this.array[i]);
+      newArr.push(newEL);
+    }
+
+    return newArr;
+  }
+  filter(cb) {
+    const newArr = [];
+    for (let i = 0; i < this.array.length; i++) {
+      if (cb(this.array[i])) {
+        newArr.push(this.array[i]);
+      }
+    }
+    return newArr;
+  }
+  find(cb) {
+    for (let i = 0; i < this.array.length; i++) {
+      if (cb(this.array[i])) {
+        return this.array[i];
+      }
+    }
+
+    return [];
+  }
+  slice(start, end = this.array.length) {
+    if (start < 0) start += this.array.length;
   }
 }
